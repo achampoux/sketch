799a49899d2e970d4a4580b58c3840f6707319be
achampoux <22298638+achampoux@users.noreply.github.com>
2023-08-27 14:49:44 -0400

    Added grid generator with user prompt

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var grid = document.getElementById('sketch-grid');
 
-for (let index = 1; index <= 256; index++) {
+function generateGrid(rows){
+
+grid.style.cssText = "grid-template-columns: repeat(" + rows + ", 1fr);grid-template-rows: repeat(" + rows + ", 1fr);";
+
+let totalCells = rows * rows;
+grid.innerHTML = '';
+
+for (let index = 1; index <= totalCells; index++) {
     grid.innerHTML += '<div class="grid-element"></div>';
 }
 
@@ -9,3 +16,18 @@ var gridElement = document.getElementsByClassName('grid-element');
 Array.from(gridElement).forEach(element => {
     element.addEventListener('mouseover', function(){ element.style.cssText = "background-color:black;"});
 });
+
+}
+
+generateGrid(16);
+
+var gridGenerator = document.getElementById('grid-generator');
+var userPrompt = 0;
+
+gridGenerator.addEventListener('click', function(){
+    userPrompt = 0;
+    while(userPrompt <= 0 || userPrompt > 100){
+    userPrompt = prompt("How many rows for the new grid? (1-100)");
+    generateGrid(userPrompt);
+    }
+})
