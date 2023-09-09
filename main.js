3b4d4229404bd8efb07d2751947bfb388f3de634
achampoux <22298638+achampoux@users.noreply.github.com>
2023-09-09 14:00:12 -0400

    Moved innerHTML outside generateGrid() for loop

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var grid = document.getElementById('sketch-grid');
 
-function generateGrid(rows){
+function generateGrid(rows) {
 
-grid.style.cssText = "grid-template-columns: repeat(" + rows + ", 1fr);grid-template-rows: repeat(" + rows + ", 1fr);";
+    grid.style.cssText = "grid-template-columns: repeat(" + rows + ", 1fr);grid-template-rows: repeat(" + rows + ", 1fr);";
 
-let totalCells = rows * rows;
-grid.innerHTML = '';
+    let totalCells = rows * rows;
+    let html = ""
 
-for (let index = 1; index <= totalCells; index++) {
-    grid.innerHTML += '<div class="grid-element"></div>';
-}
+    for (let index = 1; index <= totalCells; index++) {
+        html += '<div class="grid-element"></div>';
+    }
 
-var gridElement = document.getElementsByClassName('grid-element');
+    grid.innerHTML = html;
 
-Array.from(gridElement).forEach(element => {
-    element.addEventListener('mouseover', function(){ element.style.cssText = "background-color:black;"});
-});
+    var gridElement = document.getElementsByClassName('grid-element');
+
+    Array.from(gridElement).forEach(element => {
+        element.addEventListener('mouseover', function () { element.style.cssText = "background-color:black;" });
+    });
 
 }
 
@@ -24,10 +26,10 @@ generateGrid(16);
 var gridGenerator = document.getElementById('grid-generator');
 var userPrompt = 0;
 
-gridGenerator.addEventListener('click', function(){
+gridGenerator.addEventListener('click', function () {
     userPrompt = 0;
-    while(userPrompt <= 0 || userPrompt > 100){
-    userPrompt = prompt("How many rows for the new grid? (1-100)");
-    generateGrid(userPrompt);
+    while (isNaN(userPrompt) || userPrompt <= 0 || userPrompt > 100) {
+        userPrompt = prompt("How many rows for the new grid? (1-100)");
+        generateGrid(userPrompt);
     }
 })
